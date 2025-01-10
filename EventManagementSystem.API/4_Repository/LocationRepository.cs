3fdf67bd23df59cbe6bd0d1118e2ebab72ef4fd1
Bingrun Zheng <bingrun1204@gmail.com>
2025-01-10 10:49:59 -0500

    LocationRepo implemented

---
+++ EventManagementSystem.API/4_Repository/LocationRepository.cs
@@ -0,0 +1,49 @@
+using EventManagementSystem.API.Data;
+
+namespace EventManagementSystem.API.Repository;
+
+public class LocationRepository : ILocationRepository
+{
+    private readonly EventDbContext _context;
+
+    public LocationRepository(EventDbContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public async Task AddLocation(Location location)
+    {
+        await _context.Locations.AddAsync(location);
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteLocation(Location location)
+    {
+        var locationToDelete = await _context.Locations.FindAsync(location.LocationID);
+
+        if (locationToDelete != null)
+        {
+            _context.Locations.Remove(locationToDelete);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task<Location> GetLocationById(int locationId)
+    {
+        var location = await _context.Locations.FindAsync(locationId);
+
+        if (location == null)
+        {
+            throw new InvalidOperationException($"Location with id {locationId} not found.");
+        }
+
+        return location;
+    }
+
+    public async Task UpdateLocation(Location location)
+    {
+        _context.Locations.Update(location);
+        await _context.SaveChangesAsync();
+    }
+}
